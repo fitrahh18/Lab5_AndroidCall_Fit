84c28a92008433486a55191557c2f68c70d49756
fitrahh18 <fitrahbahsri01@gmail.com>
2021-12-02 23:10:01 +0800

    lab5

---
+++ app/src/main/java/com/example/lab5androidcall_fitrah/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lab5androidcall_fitrah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lab5acallwa.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
